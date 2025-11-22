e9b15e8dccbc07a7b6060503bf82278b85952447
Joao Secundo <alundra@academico.ufs.br>
2025-11-21 23:48:07 -0300

    Fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,15 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  base: "/",
+  base: "/MyWebsite/",
   
 })
+
+/**
+ * To serve a GitHub Pages site from the root path, you should create a repository named <username>.github.io 
+ * and place your index.html file directly in the root of that repository. This way, your site will be accessible
+ *  at https://<username>.github.io without any additional path
+ * 
+ * Só funciona com a opção Build and deployment: From a branch instead of github actions??
+*/
+// https://stackoverflow.com/questions/36782467/set-subdirectory-as-website-root-on-github-pages#36782614
